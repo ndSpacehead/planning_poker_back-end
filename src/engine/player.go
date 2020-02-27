350f6ce393d54f8cfe1078743b66af86b6037baa
SpaceHead1C <damh17@gmail.com>
2020-02-27 23:52:03 +0300

    Client is a low-level part of player

--- src/engine/player.go
+++ src/engine/player.go
@@ -2,14 +2,22 @@ package engine
 
 // Player is a user who envolved in estimation process
 type Player struct {
-	name string
-	deck *Deck
+	Client
+	name   string
+	deck   *Deck
+	room   *Room
+	doneCh chan bool
 }
 
 // NewPlayer is a constructor
-func NewPlayer(name string) *Player{
+func NewPlayer(client Client) *Player {
+	doneCh := make(chan bool)
+
 	return &Player{
-		name,
+		client,
+		"Player " + string(client.id),
 		NewDeck(),
+		nil,
+		doneCh,
 	}
 }
--- src/engine/server.go
+++ src/engine/server.go
@@ -1,6 +1,7 @@
 package engine
 
 import (
+	"log"
 	"net/http"
 
 	"golang.org/x/net/websocket"
@@ -10,6 +11,9 @@ import (
 type Server struct {
 	pattern  string
 	messages []*Message
+	clients  map[int]*Client
+	addCh    chan *Client
+	delCh    chan *Client
 	doneCh   chan bool
 	errCh    chan error
 }
@@ -17,20 +21,47 @@ type Server struct {
 // NewServer  Create new app server
 func NewServer(pattern string) *Server {
 	messages := []*Message{}
+	clients := make(map[int]*Client)
+	addCh := make(chan *Client)
+	delCh := make(chan *Client)
 	doneCh := make(chan bool)
 	errCh := make(chan error)
 
 	return &Server{
 		pattern,
 		messages,
+		clients,
+		addCh,
+		delCh,
 		doneCh,
 		errCh,
 	}
 }
 
+// Add new player
+func (s *Server) Add(c *Client) {
+	s.addCh <- c
+}
+
+// Del player go out
+func (s *Server) Del(c *Client) {
+	s.delCh <- c
+}
+
+// Done with servers work
+func (s *Server) Done() {
+	s.doneCh <- true
+}
+
+func (s *Server) Err(err error) {
+	s.errCh <- err
+}
+
 // Listen and serve.
 // It serves client connection and broadcast request.
 func (s *Server) Listen() {
+	log.Println("Listening server...")
+
 	// websocket handler
 	onConnected := func(ws *websocket.Conn) {
 		defer func() {
@@ -39,11 +70,29 @@ func (s *Server) Listen() {
 				s.errCh <- err
 			}
 		}()
+		client := NewClient(ws, s)
+		s.Add(client)
+		client.Listen()
 	}
 
 	http.Handle(s.pattern, websocket.Handler(onConnected))
+	log.Println("Created handler")
 
 	for {
-		return
+		select {
+		// Add new player
+		case c := <-s.addCh:
+			log.Println("Added new player")
+			s.clients[c.id] = c
+			log.Println("Now", len(s.clients), "players connected.")
+		// Delete a player
+		case c := <-s.delCh:
+			log.Println("Delete a player")
+			delete(s.clients, c.id)
+		case err := <-s.errCh:
+			log.Println("Error:", err.Error())
+		case <-s.doneCh:
+			return
+		}
 	}
 }
